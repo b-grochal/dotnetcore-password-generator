8910e2f2c100bf649ecf03d4ca6e2130805eed53
Bartosz <radu480@o2.pl>
2020-01-08 22:55:22 +0100

    Refactored view's code.

--- PasswordGenerator/Views/ConsoleView.cs
+++ PasswordGenerator/Views/ConsoleView.cs
@@ -15,42 +15,59 @@ namespace PasswordGenerator.Views
 
         public PasswordDetails getPasswordDetailsFromUser()
         {
-            int passwordLength;
-            PasswordType passwordType;
-            Console.Write("Enter password's length: ");
+            //int passwordLength;
+            //PasswordType passwordType;
+            //Console.Write("Enter password's length: ");
 
+            //while (!int.TryParse(Console.ReadLine(), out passwordLength) || passwordLength < 0)
+            //{
+            //    Console.WriteLine("Invalid password's length!");
+            //    Console.Write("Enter password's length: ");
+            //}
+
+            //Console.Write("Enter password's type (simple, medium or strong): ");
+
+            //while(!Enum.TryParse(Console.ReadLine(), true, out passwordType) || !Enum.IsDefined(typeof(PasswordType), passwordType))
+            //{
+            //    Console.WriteLine("Invalid password's type!");
+            //    Console.Write("Enter password's type: ");
+            //}
+
+
+            //return null;
+            PasswordDetails passwordDetails = new PasswordDetails();
+            passwordDetails.PasswordLength = getPasswordLength();
+            passwordDetails.passwordType = getPasswordType();
+            return passwordDetails;
+        }
+
+        public void printPassword(string password)
+        {
+            Console.WriteLine($"Generated password: {password}");
+        }
+
+        private int getPasswordLength()
+        {
+            int passwordLength;
+            Console.Write("Enter password's length: ");
             while (!int.TryParse(Console.ReadLine(), out passwordLength) || passwordLength < 0)
             {
                 Console.WriteLine("Invalid password's length!");
                 Console.Write("Enter password's length: ");
             }
-
-            Console.Write("Enter password's type (simple, medium or strong): ");
-
-            while(!Enum.TryParse(Console.ReadLine(), true, out passwordType) || !Enum.IsDefined(typeof(PasswordType), passwordType))
-            {
-                Console.WriteLine("Invalid password's type!");
-                Console.Write("Enter password's type: ");
-            }
-
-
-            return null;
-
-        }
-
-        public void printPassword(string password)
-        {
-            throw new NotImplementedException();
-        }
-
-        private int getPasswordLength()
-        {
-            throw new NotImplementedException();
+            return passwordLength;
         }
 
         private PasswordType getPasswordType()
         {
-            throw new NotImplementedException();
+            PasswordType passwordType;
+            Console.Write("Enter password's type (simple, medium or strong): ");
+            while (!Enum.TryParse(Console.ReadLine(), true, out passwordType) || !Enum.IsDefined(typeof(PasswordType), passwordType))
+            {
+                Console.WriteLine("Invalid password's type!");
+                Console.Write("Enter password's type: ");
+            }
+            return passwordType;
         }
     }
 }
