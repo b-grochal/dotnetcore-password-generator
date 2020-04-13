9f58d1a61f00f7777d3df1ed63921e24f154501c
Bartosz <radu480@o2.pl>
2020-04-13 20:19:18 +0200

    Implemented radnom number generator class.

---
+++ PasswordGenerator/Helpers/RandomNumberGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PasswordGenerator.Helpers
+{
+    public class RandomNumberGenerator
+    {
+        private readonly RNGCryptoServiceProvider rngCryptoServiceProvider;
+
+        public RandomNumberGenerator()
+        {
+            rngCryptoServiceProvider = new RNGCryptoServiceProvider();
+        }
+
+        public int GenerateRandomNumber(int minValue, int maxValue)
+        {
+            if (minValue > maxValue)
+                throw new ArgumentOutOfRangeException();
+
+            byte[] randomGeneratdeBytes = new byte[sizeof(int)];
+            int scale = int.MaxValue;
+
+            while(scale == int.MaxValue)
+            {
+                rngCryptoServiceProvider.GetBytes(randomGeneratdeBytes);
+                scale = BitConverter.ToInt32(randomGeneratdeBytes);
+            }
+
+            return (int)(minValue + (maxValue - minValue) * (scale / int.MaxValue));
+        }
+    }
+}
